d21e3506701490e056431bfbb98566face7ed3e6
Kei-K23 <keiksl2301@gmail.com>
2024-10-09 21:02:39 +0630

    Add GetProductById service method

---
+++ service/product_service.go
@@ -0,0 +1,32 @@
+package service
+
+import (
+	"context"
+	"log"
+
+	"github.com/Kei-K23/go-ecommerce-api/models"
+	"github.com/Kei-K23/go-ecommerce-api/repository"
+)
+
+type ProductService interface {
+	GetProductById(ctx context.Context, id int) (*models.Product, error)
+}
+
+type productService struct {
+	repo repository.ProductRepository
+}
+
+func NewProductService(repo repository.ProductRepository) ProductService {
+	return &productService{repo: repo}
+}
+
+// GetProductById implements ProductService.
+func (p *productService) GetProductById(ctx context.Context, id int) (*models.Product, error) {
+	product, err := p.repo.GetProductById(ctx, id)
+	if err != nil {
+		log.Println("Error in GetProductById:", err)
+		return nil, err
+	}
+
+	return product, nil
+}
