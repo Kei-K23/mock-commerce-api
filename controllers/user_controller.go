b625fde4fe1f58bb24f74c71594861da84f1de66
Kei-K23 <keiksl2301@gmail.com>
2024-10-11 14:57:38 +0630

    Add user controller CRUD methods

---
+++ controllers/user_controller.go
@@ -0,0 +1,121 @@
+package controllers
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/Kei-K23/go-ecommerce-api/dto"
+	"github.com/Kei-K23/go-ecommerce-api/mapper"
+	"github.com/Kei-K23/go-ecommerce-api/repository"
+	"github.com/Kei-K23/go-ecommerce-api/services"
+	"github.com/gin-gonic/gin"
+)
+
+type UserController struct {
+	service services.UserService
+}
+
+func NewUserController(service services.UserService) *UserController {
+	return &UserController{service: service}
+}
+
+func (p *UserController) CreateUser(c *gin.Context) {
+	var productRequest dto.UserRequest
+	if err := c.ShouldBindJSON(&productRequest); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	product := mapper.MatchUserRequestToUser(productRequest)
+
+	createdUser, err := p.service.CreateUser(c.Request.Context(), product)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, createdUser)
+}
+
+func (p *UserController) UpdateUser(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var productRequest dto.UserRequest
+	if err := c.ShouldBindJSON(&productRequest); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	product := mapper.MatchUserRequestToUser(productRequest)
+
+	createdUser, err := p.service.UpdateUser(c.Request.Context(), id, product)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, createdUser)
+}
+
+func (p *UserController) DeleteUser(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	deletedId, err := p.service.DeleteUser(c.Request.Context(), id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"id": deletedId})
+}
+
+func (p *UserController) GetUserById(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	product, err := p.service.GetUserById(c.Request.Context(), id)
+	if err != nil {
+		if err == repository.ErrUserNotFound {
+			c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, product)
+}
+
+func (p *UserController) GetAllUsers(c *gin.Context) {
+
+	limitStr := c.Query("limit")
+	title := c.Query("title")
+	category := c.Query("category")
+	sortBy := c.Query("sort")
+
+	products, err := p.service.GetAllUsers(c.Request.Context(), title, category, limitStr, sortBy)
+
+	if err != nil {
+		if err == repository.ErrUserNotFound {
+			c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, products)
+}
---
+++ mapper/user_mapper.go
@@ -0,0 +1,41 @@
+package mapper
+
+import (
+	"github.com/Kei-K23/go-ecommerce-api/dto"
+	"github.com/Kei-K23/go-ecommerce-api/models"
+)
+
+func MatchUserRequestToUser(req dto.UserRequest) *models.User {
+	return &models.User{
+		Username:      req.Username,
+		FirstName:     req.FirstName,
+		LastName:      req.LastName,
+		Email:         req.Email,
+		Password:      req.Password,
+		Phone:         req.Phone,
+		City:          req.City,
+		Street:        req.Street,
+		AddressNumber: req.AddressNumber,
+		ZipCode:       req.ZipCode,
+		Lat:           req.Lat,
+		Long:          req.Long,
+	}
+}
+
+func MatchUserToUserRequest(user models.User) *dto.UserRequest {
+	return &dto.UserRequest{
+		ID:            user.ID,
+		Username:      user.Username,
+		FirstName:     user.FirstName,
+		LastName:      user.LastName,
+		Email:         user.Email,
+		Password:      user.Password,
+		Phone:         user.Phone,
+		City:          user.City,
+		Street:        user.Street,
+		AddressNumber: user.AddressNumber,
+		ZipCode:       user.ZipCode,
+		Lat:           user.Lat,
+		Long:          user.Long,
+	}
+}
